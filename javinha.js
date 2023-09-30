9c5add87d1520e9f0310fa459b2709258e78c636
Omigaass <miguelanperibeiro@gmail.com>
2023-09-29 21:28:51 -0300

    V1.0

---
+++ javinha.js
@@ -0,0 +1,14 @@
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+let header = document.querySelector(".header_sec");
+
+menu.addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    header.classList.toggle("active");
+
+});
+
+window.onscroll = () => {
+    navbar.classList.remove("active");
+    header.classList.remove("active");
+};
