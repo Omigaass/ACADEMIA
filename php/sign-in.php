5d66f301752ab7a615392dd7164352b1df7fbdca
Omigaass <miguelanperibeiro@gmail.com>
2023-10-04 23:33:08 -0300

    V3.0

--- php/sign-in.php
+++ php/sign-in.php
@@ -1,34 +1,53 @@
 <?php
+    
 session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST["USER_NICKNAME"];
-    $password = $_POST["USER_PASSWORD"];
+    $user_email = $_POST['USER_EMAIL'];
+    $user_pw = $_POST['USER_PASSWORD'];
 
-    $servername = "localhost";
-    $db_username = "root";
-    $db_password = "";
-    $dbname = "triboworkout";
+    // Conexão com o banco de dados usando MySQLi
+    $mysqli = new mysqli('localhost', 'root', '', 'triboworkout');
 
-    $conn = new mysqli($servername, $db_username, $db_password, $dbname);
-
-    if ($conn->connect_error) {
-        die("Erro na conexão com o banco de dados: " . $conn->connect_error);
+    // Verificar a conexão
+    if ($mysqli->connect_error) {
+        die("Erro na conexão: " . $mysqli->connect_error);
     }
+    // Consulta SQL para obter o usuário com base no email
+    $query_select = "SELECT USER_EMAIL, USER_PASSWORD FROM user_account WHERE USER_EMAIL = ?";
+    $session_id_select = "SELECT USER_COD FROM user_account WHERE USER_EMAIL = ?";
+    $stmt = $mysqli->prepare($query_select);
+    $stmt->bind_param("s", $user_email);
 
-    $sql = "SELECT * FROM USER_ACCOUNT WHERE USER_NICKNAME = '$username' AND USER_PASSWORD = '$password'";
-    $result = $conn->query($sql);
+    // Executar a consulta
+    $stmt->execute();
 
-    if ($result->num_rows == 1) {
-        $_SESSION["USER_NICKNAME"] = $username;
-        header("Location: home.html");
-        exit();
-    } else {
-        if (isset($erro)) {
-            exibirPopupErro($erro);
+    // Obter o resultado da consulta
+    $result = $stmt->get_result();
+
+    // Verificar se o usuário existe
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $hashed_password = $row['USER_PASSWORD'];
+
+        // Verificar a senha
+        if (password_verify($user_pw, $hashed_password)) {
+            // Senha correta, redirecionar para a página de sucesso
+            $_SESSION['USER_ID'] = $session_id_select;
+            header('Location: ../usuario.html');
+        } else {
+            // Senha incorreta
+            echo "<script language='javascript' type='text/javascript'>
+            alert('Senha incorreta');window.location.href='../index.html';</script>";
         }
+    } else {
+        // Usuário não encontrado
+        echo "<script language='javascript' type='text/javascript'>
+        alert('Usuário não encontrado');window.location.href='../index.html';</script>";
     }
 
-    $conn->close();
+    // Fechar a conexão
+    $stmt->close();
+    $mysqli->close();
 }
 ?>
--- php/sign-up.php
+++ php/sign-up.php
@@ -1,43 +1,63 @@
 <?php
-session_start();
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST["USER_NICKNAME"];
-    $email = $_POST["USER_EMAIL"];
-    $password = $_POST["USER_PASSWORD"];
+    $user_name = $_POST['USER_NICKNAME'];
+    $user_pw = $_POST['USER_PASSWORD'];
+    $user_email = $_POST['USER_EMAIL'];
 
-    if (empty($username) || empty($email) || empty($password)) {
-        $erro = "Todos os campos devem ser preenchidos.";
-    } else {
-        $servername = "localhost";
-        $db_username = "root";
-        $db_password = "";
-        $dbname = "triboworkout";
+    // Conexão com o banco de dados usando MySQLi
+    $mysqli = new mysqli('localhost', 'root', '', 'triboworkout');
 
-        $conn = new mysqli($servername, $db_username, $db_password, $dbname);
-
-        if ($conn->connect_error) {
-            die("Erro na conexão com o banco de dados: " . $conn->connect_error);
-        }
-
-        $check_username_sql = "SELECT * FROM USER_ACCOUNT WHERE USER_NICKNAME = '$username'";
-        $check_username_result = $conn->query($check_username_sql);
-
-        if ($check_username_result->num_rows > 0) {
-            $erro = "Nome de usuário já está em uso. Escolha outro nome de usuário.";
-        } else {
-            $insert_sql = "INSERT INTO USER_ACCOUNT (USER_NICKNAME, USER_EMAIL, USER_PASSWORD)
-                            VALUES ('$username', '$email', '$password')";
-
-            if ($conn->query($insert_sql) === TRUE) {
-                $_SESSION["USER_NICKNAME"] = $username;
-                header("Location: home.html");
-                exit();
-            } else {
-                $erro = "Erro ao cadastrar usuário: " . $conn->error;
-            }
-        }
-        $conn->close();
+    // Verificar a conexão
+    if ($mysqli->connect_error) {
+        die("Erro na conexão: " . $mysqli->connect_error);
     }
+
+    // Consulta SQL com prepared statement para evitar SQL injection
+    $query_select = "SELECT USER_NICKNAME FROM user_account WHERE USER_NICKNAME = ?";
+    $session_id_select = "SELECT USER_COD FROM user_account WHERE USER_EMAIL = ?";
+    $stmt = $mysqli->prepare($query_select);
+    $stmt->bind_param("s", $user_name);
+
+    // Executar a consulta
+    $stmt->execute();
+
+    // Obter o resultado da consulta
+    $result = $stmt->get_result();
+
+    // Verificar se o usuário já existe
+    if ($result->num_rows > 0) {
+        // Usuário já existe
+        echo "<script language='javascript' type='text/javascript'>
+        alert('Esse login já existe');window.location.href='../index.html';</script>";
+        die();
+    } else {
+        // Usuário não existe, vamos inseri-lo no banco de dados
+
+        // Consulta SQL para inserir usuário
+        $query_insert = "INSERT INTO user_account (USER_NICKNAME, USER_PASSWORD, USER_EMAIL) VALUES (?, ?, ?)";
+        $stmt_insert = $mysqli->prepare($query_insert);
+
+        // Hash da senha
+        $hashed_password = password_hash($user_pw, PASSWORD_DEFAULT);
+
+        // Bind dos parâmetros
+        $stmt_insert->bind_param("sss", $user_name, $hashed_password, $user_email);
+
+        // Executar a inserção
+        if ($stmt_insert->execute()) {
+            // Sucesso no cadastro
+            $_SESSION['USER_ID'] = $session_id_select;
+            echo "<script language='javascript' type='text/javascript'>
+            alert('Usuário cadastrado com sucesso!');window.location.href='../usuario.html';</script>";
+        } else {
+            // Erro na inserção
+            echo "<script language='javascript' type='text/javascript'>
+            alert('Não foi possível cadastrar esse usuário');window.location.href='../index.html';</script>";
+        }
+    }
+
+    // Fechar a conexão
+    $stmt->close();
+    $stmt_insert->close();
+    $mysqli->close();
 }
-?>
